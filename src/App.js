417102e655bab8f462fcc6a84c05cd297810829d
SuvankarDev <suvankarchowdhury549@gmail.com>
2024-06-28 00:51:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import './App.css';
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Quiz from './components/Quiz';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <Router>
+      <div className='App'>
+        <Header />
+        <Routes>
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
